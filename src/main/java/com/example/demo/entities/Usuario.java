168527d6c5f665c98f6055f0c73a05cca239d209
Daniel Morais <damorais@gmail.com>
2019-10-02 16:41:51 -0300

    Iniciando uma aplicação spring mvc

---
+++ src/main/java/com/example/demo/entities/Usuario.java
@@ -0,0 +1,53 @@
+package com.example.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@NotBlank(message = "O nome é obrigatório")
+	private String nome;
+	@NotBlank(message = "O sobrenome é obrigatório")
+	private String sobrenome;
+	
+	public long getId() {
+		return this.id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public Usuario() {}
+	
+	public Usuario(String nome, String sobrenome) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+	}
+	
+	public Usuario(long id, String nome, String sobrenome) {
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+	}
+}
